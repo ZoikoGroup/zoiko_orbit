449ccc8aed1553a2605b6b32d0ede20ed0366bb4
indra <indra@zoikogroup.com>
2026-02-25 14:42:34 +0530

    testing ci/cd pipeline

--- src/pages/plans.jsx
+++ src/pages/plans.jsx
@@ -1,3 +1,7 @@
 export default function Plans() {
-  return <div><h1>Plans Page</h1></div>;
+  return (
+    <div>
+      <h1>PLANS PAGE</h1>
+    </div>
+  );
 }
--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -1,3 +1,7 @@
 export default function Support() {
-  return <div><h1>Support Page</h1></div>;
+  return (
+    <div>
+      <h1>SUPPORT PAGE</h1>
+    </div>
+  );
 }
